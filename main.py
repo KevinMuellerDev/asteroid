48c4d06537413eaf75aae59b474d203c4daf3458
Kevin Mueller <kevin.mueller@tuta.com>
2025-06-02 17:37:39 +0200

    add: Player class with roation and update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -7,23 +9,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
         screen.fill(color="black")
+        player.update(dt)
+        player.draw(screen)
 
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
-
-
-    
 
 
 if __name__ == "__main__":
