ffb80965513eb255accc032445a5840909487c56
Kevin Mueller <kevin.mueller@tuta.com>
2026-09-19 12:52:50 +0200

    add: updating and drawing of sprites

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main() -> None:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt: float = 0.0
+    # GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # PLAYER
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -19,7 +24,10 @@ def main() -> None:
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
